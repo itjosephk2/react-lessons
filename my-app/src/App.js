2c8ebec5cbef93e1b7d833acb8d5a96eeaac72cd
Joseph Keane <josephkeane996@gmail.com>
2024-08-19 15:30:48 +0000

    Methods as Props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,12 +8,13 @@ import './App.css';
 // import EventBindings from './components/EventBinding';
 // import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 // import ConditionalRenderingClass from './components/ConditionalRenderingClass';
-import NestingCompenents from './components/NestingComponents';
+// import NestingCompenents from './components/NestingComponents';
+import MethodAsPropsParent from './components/MethodAsPropsParent';
 
 function App() {
   return (
     <div className="App">
-      <NestingCompenents />
+       <MethodAsPropsParent />
     </div>
   );
 }
