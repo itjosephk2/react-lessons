cf355f624c7c0b2274881177df4c9e0de8c1b526
Joseph Keane <josephkeane996@gmail.com>
2024-08-19 09:30:29 +0000

    Lesson 1

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const FunctionalGreeting = () => <h1>Hello from React!</h1>
+
+
+export default FunctionalGreeting
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function FunctionalGreetingWithProps(props) {
+  return (
+    <h1>Hello From React</h1>>
+    <p>With Props</p>
+  )
+}
+
+FunctionalGreetingWithProps.propTypes = {}
+
+export default FunctionalGreetingWithProps
