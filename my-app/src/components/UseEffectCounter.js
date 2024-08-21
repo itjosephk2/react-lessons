31924037009c8eca6f89abdecee024bbf4c475bd
Joseph Keane <josephkeane996@gmail.com>
2024-08-21 10:24:55 +0000

    Hook timer - not working

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -2,29 +2,28 @@ import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
+
     useEffect(() => {
-        console.log(count)
+        console.log('count 1 effect');
         document.title = count;
     }, [count]);
-    const [count2, setCount2] = useState(0);
-    useEffect(() => {
-        console.log(count2)
-        document.title = count;
-    }, [count2]);
-  return (
-    <div>
-        <button onClick = {
-            () => setCount(count => count + 1)
-        }>
-            Increment Count ({count})
-        </button>
-        <button onClick = {
-            () => setCount2(count2 => count2 + 1)
-        }>
-            Increment Count2 ({count2})
-        </button>
-    </div>
-  )
+    useEffect(()=>{
+        console.log('Creating timer');
+        const interval = setInterval(() => {
+            setTime(time => time + .5)
+        }, 1000);
+    }, []);
+    return (
+        <div>
+            <button onClick={
+                () => setCount(count => count + 1)
+            }>
+                Increment Count ({count})
+            </button>
+            <h2>Time is {time}</h2>
+        </div>
+    )
 }
 
 export default UseEffectCounter
